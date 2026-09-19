c1c4e4538dfd38a8ff34f8e97307bd8651bcba44
DAI\dhruba.singha <dhruba.singha@williamoneilindia.com>
2026-09-19 21:19:55 +0530

    feat(core): add listing reference value object

--- src/northstar_core/domain/value_objects/__init__.py
+++ src/northstar_core/domain/value_objects/__init__.py
@@ -1,15 +1,18 @@
 """Reusable Core Domain Value Objects."""
 
+from .listing_reference import InvalidListingReferenceError, ListingReference
 from .listing_status import InvalidListingStatusError, ListingStatus
 from .participant_identity import InvalidParticipantIdentityError, ParticipantIdentity
 from .participant_reference import InvalidParticipantReferenceError, ParticipantReference
 from .tradability import InvalidTradabilityError, Tradability
 
 __all__ = [
+    "InvalidListingReferenceError",
     "InvalidListingStatusError",
     "InvalidParticipantIdentityError",
     "InvalidParticipantReferenceError",
     "InvalidTradabilityError",
+    "ListingReference",
     "ListingStatus",
     "ParticipantIdentity",
     "ParticipantReference",
